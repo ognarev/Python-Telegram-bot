1214b6f3b395c4923fbfaf6f52f50f347a24da9c
ognarev <ognarev@gmail.com>
2021-07-25 21:49:26 +0300

    separated .json for bot answers created

--- your_first_bot.py
+++ your_first_bot.py
@@ -1,11 +1,14 @@
 import json
 import telegram.error
-from telegram.ext import Updater, dispatcher
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, dispatcher, CommandHandler
+from telegram.ext.filters import Filters
+from telegram.ext.messagehandler import MessageHandler
 
 with open('.sensdata/tokens.json') as reader:
     data = json.load(reader)
     TELEGRAM_TOKEN = data['TELEGRAM_TOKEN']
+with open('data/bot_answers.json') as reader:
+    bot_answers = json.load(reader)
 
 try:
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
@@ -15,17 +18,32 @@ except telegram.error.TelegramError:
 except Exception:
     print("Oops, can't get updates from Telegram bot but it's not Telegram error.")
 
-def start(update, context): # Bot action on /start command
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, write something.")
+def start_c(update, context): # reaction on /start
+    context.bot.send_message(chat_id=update.effective_chat.id, text=bot_answers["start"])
+
+def help_c(update, context): # reaction on /help
+    context.bot.send_message(chat_id=update.effective_chat.id, text=bot_answers["help"])
+
+def unknown_c(update, context): # reaction on unknown commands
+    context.bot.send_message(chat_id=update.effective_chat.id, text=bot_answers["unknown"])
+
+def message_(update, context): # reaction on text message from user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=bot_answers["message"])
 
 # Handlers & Dispatchers
 dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-help_handler = CommandHandler('help', start)
-settings_handler = CommandHandler('settings', start)
+start_handler = CommandHandler('start', start_c)
+help_handler = CommandHandler('help', help_c)
+settings_handler = CommandHandler('settings', help_c)
+message_handler = MessageHandler(~Filters.command, message_)
+unknown_handler = MessageHandler(Filters.command, unknown_c)
+
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(help_handler)
 dispatcher.add_handler(settings_handler)
+dispatcher.add_handler(message_handler)
+dispatcher.add_handler(unknown_handler) #must be last handler
 
 # Run the bot
 updater.start_polling()
+updater.idle() # You can stop the bot with Ctrl+C 
